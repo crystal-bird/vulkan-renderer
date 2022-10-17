544861ffd6d7d272f318e2404256b8c5a669c063
11blankball <filsamare@gmail.com>
2022-10-17 16:55:38 +0700

    Swapchain resize

--- src/main.c
+++ src/main.c
@@ -426,55 +426,6 @@ VkRenderPass createRenderPass(VkDevice device, VkFormat format)
     return renderPass;
 }
 
-VkSwapchainKHR createSwapchain(VkDevice device, VkSurfaceKHR surface, uint32_t familyIndex, VkFormat format, uint32_t width, uint32_t height)
-{
-    const VkSwapchainCreateInfoKHR createInfo =
-    {
-        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
-        .surface = surface,
-        .minImageCount = 2,
-        .imageFormat = format,
-        .imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
-        .imageExtent.width = width,
-        .imageExtent.height = height,
-        .imageArrayLayers = 1,
-        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
-        .queueFamilyIndexCount = 1,
-        .pQueueFamilyIndices = &familyIndex,
-        .preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
-        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
-        .presentMode = VK_PRESENT_MODE_FIFO_KHR,
-    };
-
-    VkSwapchainKHR swapchain = 0;
-    VK_CHECK(vkCreateSwapchainKHR(device, &createInfo, 0, &swapchain));
-
-    return swapchain;
-}
-
-VkSemaphore createSemaphore(VkDevice device)
-{
-    const VkSemaphoreCreateInfo createInfo = 
-    {
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-    };
-
-    VkSemaphore semaphore = 0;
-    VK_CHECK(vkCreateSemaphore(device, &createInfo, 0, &semaphore));
-
-    return semaphore;
-}
-
-VkImage* getSwapchainImages(VkDevice device, VkSwapchainKHR swapchain, uint32_t* pSwapchainImageCount)
-{
-    VK_CHECK(vkGetSwapchainImagesKHR(device, swapchain, pSwapchainImageCount, 0));
-    
-    VkImage* swapchainImages = calloc(*pSwapchainImageCount, sizeof(*swapchainImages));
-    VK_CHECK(vkGetSwapchainImagesKHR(device, swapchain, pSwapchainImageCount, swapchainImages));
-
-    return swapchainImages;
-}
-
 VkImageView createImageView(VkDevice device, VkImage image, VkFormat format)
 {
     const VkImageViewCreateInfo createInfo =
@@ -511,6 +462,112 @@ VkFramebuffer createFramebuffer(VkDevice device, VkRenderPass renderPass, VkImag
     VK_CHECK(vkCreateFramebuffer(device, &createInfo, 0, &framebuffer));
 
     return framebuffer;
+}
+
+typedef struct
+{
+    VkSwapchainKHR  swapchain;
+
+    uint32_t        width, height;
+    uint32_t        imageCount;
+
+    VkImage*        images;
+    VkImageView*    imageViews;
+    VkFramebuffer*  framebuffers;
+} Swapchain;
+
+void createSwapchain(Swapchain* swapchain, VkDevice device, VkSurfaceKHR surface, uint32_t familyIndex, VkFormat format, uint32_t width, uint32_t height, VkRenderPass renderPass, VkSwapchainKHR oldSwapchain)
+{
+    const VkSwapchainCreateInfoKHR createInfo =
+    {
+        .sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR,
+        .surface = surface,
+        .minImageCount = 2,
+        .imageFormat = format,
+        .imageColorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR,
+        .imageExtent.width = width,
+        .imageExtent.height = height,
+        .imageArrayLayers = 1,
+        .imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT,
+        .queueFamilyIndexCount = 1,
+        .pQueueFamilyIndices = &familyIndex,
+        .preTransform = VK_SURFACE_TRANSFORM_IDENTITY_BIT_KHR,
+        .compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR,
+        .presentMode = VK_PRESENT_MODE_FIFO_KHR,
+        .oldSwapchain = oldSwapchain,
+    };
+
+    VK_CHECK(vkCreateSwapchainKHR(device, &createInfo, 0, &swapchain->swapchain));
+    assert(swapchain->swapchain);
+
+    VK_CHECK(vkGetSwapchainImagesKHR(device, swapchain->swapchain, &swapchain->imageCount, 0));
+
+    swapchain->images = calloc(swapchain->imageCount, sizeof(*swapchain->images));
+    VK_CHECK(vkGetSwapchainImagesKHR(device, swapchain->swapchain, &swapchain->imageCount, swapchain->images));
+
+    swapchain->imageViews = calloc(swapchain->imageCount, sizeof(*swapchain->imageViews));
+    for (uint32_t i = 0; i < swapchain->imageCount; i++)
+    {
+        swapchain->imageViews[i] = createImageView(device, swapchain->images[i], format);
+        assert(swapchain->imageViews[i]);
+    }
+
+    swapchain->framebuffers = calloc(swapchain->imageCount, sizeof(*swapchain->framebuffers));
+    for (uint32_t i = 0; i < swapchain->imageCount; i++)
+    {
+        swapchain->framebuffers[i] = createFramebuffer(device, renderPass, swapchain->imageViews[i], width, height);
+        assert(swapchain->framebuffers[i]);
+    }
+
+    swapchain->width = width;
+    swapchain->height = height;
+}
+
+void destroySwapchain(VkDevice device, Swapchain* swapchain)
+{
+    for (uint32_t i = 0; i < swapchain->imageCount; i++)
+    {
+        vkDestroyFramebuffer(device, swapchain->framebuffers[i], 0);
+        vkDestroyImageView(device, swapchain->imageViews[i], 0);
+    }
+    free(swapchain->framebuffers);
+    free(swapchain->imageViews);
+    free(swapchain->images);
+
+    vkDestroySwapchainKHR(device, swapchain->swapchain, 0);
+}
+
+void resizeSwapchainIfNecessary(Swapchain* swapchain, VkPhysicalDevice physicalDevice, VkDevice device, VkSurfaceKHR surface, uint32_t familyIndex, VkFormat format, VkRenderPass renderPass)
+{
+    VkSurfaceCapabilitiesKHR surfaceCaps;
+    VK_CHECK(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(physicalDevice, surface, &surfaceCaps));
+
+    uint32_t newWidth = surfaceCaps.currentExtent.width;
+    uint32_t newHeight = surfaceCaps.currentExtent.height;
+
+    if (swapchain->width == newWidth && swapchain->height == newHeight)
+        return;
+
+    Swapchain old = *swapchain;
+
+    createSwapchain(swapchain, device, surface, familyIndex, format, newWidth, newHeight, renderPass, old.swapchain);
+
+    VK_CHECK(vkDeviceWaitIdle(device));
+
+    destroySwapchain(device, &old);
+}
+
+VkSemaphore createSemaphore(VkDevice device)
+{
+    const VkSemaphoreCreateInfo createInfo = 
+    {
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+    };
+
+    VkSemaphore semaphore = 0;
+    VK_CHECK(vkCreateSemaphore(device, &createInfo, 0, &semaphore));
+
+    return semaphore;
 }
 
 VkCommandPool createCommandPool(VkDevice device, uint32_t familyIndex)
@@ -584,8 +641,8 @@ int main(int argc, char* argv[])
     int windowWidth, windowHeight;
     glfwGetWindowSize(window, &windowWidth, &windowHeight);
 
-    VkSwapchainKHR swapchain = createSwapchain(device, surface, familyIndex, surfaceFormat, windowWidth, windowHeight);
-    assert(swapchain);
+    Swapchain swapchain;
+    createSwapchain(&swapchain, device, surface, familyIndex, surfaceFormat, windowWidth, windowHeight, renderPass, 0);
 
     VkSemaphore acquireSemaphore = createSemaphore(device);
     assert(acquireSemaphore);
@@ -595,24 +652,6 @@ int main(int argc, char* argv[])
 
     VkQueue queue = 0;
     vkGetDeviceQueue(device, familyIndex, 0, &queue);
-
-    uint32_t swapchainImageCount = 0;
-    VkImage* swapchainImages = getSwapchainImages(device, swapchain, &swapchainImageCount);
-    assert(swapchainImages);
-
-    VkImageView* swapchainImageViews = calloc(swapchainImageCount, sizeof(*swapchainImageViews));
-    for (uint32_t i = 0; i < swapchainImageCount; i++)
-    {
-        swapchainImageViews[i] = createImageView(device, swapchainImages[i], surfaceFormat);
-        assert(swapchainImageViews[i]);
-    }
-
-    VkFramebuffer* swapchainFramebuffers = calloc(swapchainImageCount, sizeof(*swapchainFramebuffers));
-    for (uint32_t i = 0; i < swapchainImageCount; i++)
-    {
-        swapchainFramebuffers[i] = createFramebuffer(device, renderPass, swapchainImageViews[i], windowWidth, windowHeight);
-        assert(swapchainFramebuffers[i]);
-    }
 
     VkCommandPool commandPool = createCommandPool(device, familyIndex);
     assert(commandPool);
@@ -634,8 +673,17 @@ int main(int argc, char* argv[])
     {
         glfwPollEvents();
 
+        glfwGetWindowSize(window, &windowWidth, &windowHeight);
+        while (windowWidth == 0 || windowHeight == 0)
+        {
+            glfwGetWindowSize(window, &windowWidth, &windowHeight);
+            glfwWaitEvents();
+        }
+
+        resizeSwapchainIfNecessary(&swapchain, physicalDevice, device, surface, familyIndex, surfaceFormat, renderPass);
+
         uint32_t imageIndex = 0;
-        VK_CHECK(vkAcquireNextImageKHR(device, swapchain, ~0ull, acquireSemaphore, 0, &imageIndex));
+        VK_CHECK(vkAcquireNextImageKHR(device, swapchain.swapchain, ~0ull, acquireSemaphore, 0, &imageIndex));
         VK_CHECK(vkResetCommandPool(device, commandPool, 0));
 
         const VkCommandBufferBeginInfo beginInfo =
@@ -653,17 +701,17 @@ int main(int argc, char* argv[])
         {
             .sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO,
             .renderPass = renderPass,
-            .framebuffer = swapchainFramebuffers[imageIndex],
-            .renderArea.extent.width = windowWidth,
-            .renderArea.extent.height = windowHeight,
+            .framebuffer = swapchain.framebuffers[imageIndex],
+            .renderArea.extent.width = swapchain.width,
+            .renderArea.extent.height = swapchain.height,
             .clearValueCount = 1,
             .pClearValues = &clearColor,
         };
 
         vkCmdBeginRenderPass(commandBuffer, &passBeginInfo, VK_SUBPASS_CONTENTS_INLINE);
 
-        VkViewport viewport = { 0, 0, (float) windowWidth, (float) windowHeight, 0, 1 };
-        VkRect2D scissor = { {0, 0}, {windowWidth, windowHeight} };
+        VkViewport viewport = { 0, 0, (float) swapchain.width, (float) swapchain.height, 0, 1 };
+        VkRect2D scissor = { {0, 0}, {swapchain.width, swapchain.height} };
 
         vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, trianglePipeline);
 
@@ -698,7 +746,7 @@ int main(int argc, char* argv[])
             .waitSemaphoreCount = 1,
             .pWaitSemaphores = &releaseSemaphore,
             .swapchainCount = 1,
-            .pSwapchains = &swapchain,
+            .pSwapchains = &swapchain.swapchain,
             .pImageIndices = &imageIndex,
         };
 
@@ -713,20 +761,7 @@ int main(int argc, char* argv[])
     vkDestroySemaphore(device, acquireSemaphore, 0);
     vkDestroySemaphore(device, releaseSemaphore, 0);
 
-    for (uint32_t i = 0; i < swapchainImageCount; i++)
-    {
-        vkDestroyFramebuffer(device, swapchainFramebuffers[i], 0);
-    }
-    free(swapchainFramebuffers);
-
-    for (uint32_t i = 0; i < swapchainImageCount; i++)
-    {
-        vkDestroyImageView(device, swapchainImageViews[i], 0);
-    }
-    free(swapchainImageViews);
-
-    free(swapchainImages);
-    vkDestroySwapchainKHR(device, swapchain, 0);
+    destroySwapchain(device, &swapchain);
 
     vkDestroyPipeline(device, trianglePipeline, 0);
     vkDestroyPipelineLayout(device, triangleLayout, 0);
